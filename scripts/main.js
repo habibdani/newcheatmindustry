0141d6fee61b77b80ef9d73cd33c257d417039f3
WDTI0\muhammad.alhamdani <muhammad.alhamdani@primecash.co.id>
2025-05-16 19:45:55 +0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+//Mod
+var mod = Vars.mods.locateMod("cheat");
+mod.meta.displayName = "[yellow]Cheat[orange]MOD";
+mod.meta.description = "@mod.cheat.description";
+mod.meta.author = "[#ff0][#fff]paulieg626";
+
+//Block
+require("blocks/liquid");
+require("blocks/power");
+require("blocks/wall");
+require("blocks/turret");
+require("blocks/value");
+require("blocks/magic");
+require("blocks/conv");
+require("blocks/shield");
+require("blocks/boost");
+require("blocks/cc");
+require("blocks/item");
+require("blocks/skip");
+require("blocks/command");
+
+print("☆ CheatMod by paulieg626 ☆");
